4b9a5f54cf449598cf25f9157210bc154f96156c
WendellGB26 <wengb159753@gmail.com>
2024-05-05 14:18:21 -0600

    feat: aws connection and new migration

--- migrations/20240425203509_create_necklaces_table.js
+++ migrations/20240425203509_create_necklaces_table.js
@@ -2,6 +2,7 @@ exports.up = function(knex) {
     return knex.schema.createTable('necklaces', function(table) {
       table.increments('id').primary();
       table.string('description').notNullable();
+      table.integer('price').notNullable();
       table.string('size').notNullable();
       table.string('color');
       table.string('gender').notNullable();
--- migrations/20240425214618_create_earrings_table.js
+++ migrations/20240425214618_create_earrings_table.js
@@ -2,6 +2,7 @@ exports.up = function(knex) {
     return knex.schema.createTable('earrings', function(table) {
       table.increments('id').primary();
       table.string('description').notNullable();
+      table.integer('price').notNullable();
       table.string('color');
       table.string('gender').notNullable();
       table.string('imageKey').notNullable();
